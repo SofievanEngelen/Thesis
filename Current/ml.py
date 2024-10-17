aab261bf25f599131dbbacfa2e68ec9c1f6e7942
SofievanEngelen <128477751+SofievanEngelen@users.noreply.github.com>
2024-10-17 19:47:54 +0100

    continued model skeleton code

--- Current/ml.py
+++ Current/ml.py
@@ -1,17 +1,21 @@
+import os
+import pickle
 from collections import Counter
 
+import sklearn
 from imblearn.over_sampling import SMOTE
-import constants
+import xgboost as xgb
+from constants import *
 
 
 def train_model(X, y, model: str, features: str, smote: bool = True, verbose: bool = True):
     match features:
         case "G":
-            X = X[X.columns.intersection(constants.GLOBAL_FEATURES)]
+            X = X[X.columns.intersection(GLOBAL_FEATURES)]
         case "L":
-            X = X[X.columns.intersection(constants.LOCAL_FEATURES)]
+            X = X[X.columns.intersection(LOCAL_FEATURES)]
         case "G+L":
-            feature_list = constants.GLOBAL_FEATURES + constants.LOCAL_FEATURES
+            feature_list = GLOBAL_FEATURES + LOCAL_FEATURES
             X = X[X.columns.intersection(feature_list)]
         case _:
             raise ValueError(f"{features} is not a valid feature set.")
@@ -26,12 +30,27 @@ def train_model(X, y, model: str, features: str, smote: bool = True, verbose: bo
         if verbose:
             print(f'Resampled dataset shape {Counter(y)}', "\nFinished SMOTE.")
 
-    match model:
+    # match model:
+
+
+
+def get_model(model_name):
+
+    model_path = f"{model_name}_model.pkl"
+
+    if os.path.isfile(model_path):
+        with open(model_path, 'rb') as f:
+            return pickle.load(f)
+
+    match model_name:
         case "XGBoost":
-            param = {'max_depth': 2, 'eta': 1, 'objective': 'binary:logistic'}
-            
+            if os.path.isfile("xgboost_model.pkl"):
+                # model =  # READ MODEL IN FROM PICKLE FILE
+
             pass
         case "LogReg":
+            params = {'penalty': 'l2', 'C': 1.0, }
+            sklearn.linear_model.LogisticRegression(params)
             pass
         case "RandomForest":
             pass
@@ -40,4 +59,23 @@ def train_model(X, y, model: str, features: str, smote: bool = True, verbose: bo
         case "NaiveBayes":
             pass
         case _:
+            raise ValueError(f"{model_name} is not a valid model.")
+
+
+def cross_validate_parameters(model):
+    match model:
+        case "XGBoost":
+            clf = xgb.XGBClassifier(XGB_PARAMS)
+        case "LogReg":
+            clf = sklearn.linear_model.LogisticRegression(LR_PARAMS)
+        case "RandomForest":
+            clf = sklearn.ensemble.RandomForestClassifier(RF_PARAMS)
+        case "linearSVM":
+            clf = sklearn.svm.LinearSVC(LSVM_PARAMS)
+        case "NaiveBayes":
+            clf = sklearn.naive_bayes.GaussianNB(NB_PARAMS)
+        case _:
             raise ValueError(f"{model} is not a valid model.")
+
+
+
