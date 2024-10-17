7b344b777dbfb0d53024be755978059546ea6d74
SofievanEngelen <128477751+SofievanEngelen@users.noreply.github.com>
2024-10-17 19:47:42 +0100

    compute features train windows

--- Current/main.py
+++ Current/main.py
@@ -1,12 +1,16 @@
-# import json
-# import random
-# import time
-import numpy as np
-import pandas as pd
-from matplotlib import pyplot as plt
+import time
 
-from windows import start_sliding_window
+import pandas as pd
+from IPython.core.display_functions import display
+from matplotlib import pyplot as plt
+from pandas import unique
+
+# from sklearn.datasets import make_classification
+
+from windows import training_windows
 from preprocessing import preprocess_gaze_data
+from constants import *
+from feature_extraction import compute_features
 
 
 def plot_gazes(participants, AOI_df, df):
@@ -31,16 +35,46 @@ def plot_gazes(participants, AOI_df, df):
 
 
 def main():
+    pass
     # Preprocessing
-    # preprocess_gaze_data("./original-test-data/raw_gaze_data.csv", to_file='processed_gaze_data.csv')
+    processed_gaze_data = preprocess_gaze_data("./original-data/raw_gaze_data.csv", to_file='processed-data/processed_gaze_data.csv')
+
+    # Feature extraction
+    # compute_features(processed_gaze_data)
+
+    # df = pd.read_csv('processed-data/test_window.csv')
+    # print(compute_features(df))
+    # pd.DataFrame.append()
+    # df = pd.read_csv('processed-data/processed_gaze_data.csv')
+    # train_windows = []
+    # for p in range(1, len(unique(df['Participant'])) + 1):
+    #     print(p)
+    #     p_df = df[df['Participant'] == p]
+    #     p_window_df = training_windows(p_df, WINDOW_SIZE)
+    #     display(p_window_df)
+    #     train_windows.append(p_window_df)
+    #
+    # train_windows_df = pd.concat(train_windows)
+    # train_windows_df.to_csv('train_windows_features.csv', index=False, header=True)
+
+    # X, y = make_classification(n_classes=2, class_sep=2,
+    #                            weights=[0.1, 0.9], n_informative=3, n_redundant=1, flip_y=0,
+    #                            n_features=20, n_clusters_per_class=1, n_samples=1000, random_state=10)
+    # train_model(X, y, "XGBoost", features="G")
+
+    # train_windows_df = training_windows('./processed_gaze_data.csv', constants.WINDOW_SIZE, 'train_windows.csv')
+    # events_df, saccade_df = detect_events(df)
+    # print(events_df)
+    # events_df.to_csv("events_test.csv")
+    # saccade_df.to_csv("sac_test.csv")
+    # print(compute_global_features(events_df, saccade_df))
 
     # Windows
-    gaze_data = pd.read_csv("processed_gaze_data.csv")
-    start_sliding_window(1, data=gaze_data, window_size=20000)  # Window size in milliseconds => 20 seconds
+    # start_sliding_window(1, data=processed_gaze_data, window_size=WINDOW_SIZE)  # Window size in milliseconds => 20 seconds
 
     # ML
     # data = pd.read_csv('training/eyetracking_by_event.csv')
-    # train_model(data)
+    # train_model(data, model)
 
 
 if __name__ == "__main__":
