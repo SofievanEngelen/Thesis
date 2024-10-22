b901594fa858ac676c13a7a2f97d61dad7d39a84
SofievanEngelen <128477751+SofievanEngelen@users.noreply.github.com>
2024-10-22 17:51:38 +0100

    refactor feature_extraction.py and drop windows if fixation lasts longer than given duration

--- constants.py
+++ constants.py
@@ -1,16 +1,17 @@
 import pandas as pd
 
 WINDOW_SIZE = 20000
+MIN_FIXATION_DURATION = 10000
 GLOBAL_FEATURES = ["A"]
 LOCAL_FEATURES = ["B"]
 
 # Dataframes
-# AOI_DF = pd.read_csv('./original-Data/aoi-boxes.csv', delimiter=';', decimal=',')
-# RAW_GAZE_DF = pd.read_csv('./original-Data/raw_gaze_data.csv')
-# PRC_GAZE_DF = pd.read_csv('./processed-Data/processed_gaze_data.csv')
+AOI_DF_PATH = './original-Data/aoi-boxes.csv'
+RAW_GAZE_DF_PATH = '/Users/sofie/dev/Python/Uni/Thesis/Thesis - code/Data/original-data/raw_gaze_data.csv'
+PRC_GAZE_DF_PATH = '/Users/sofie/dev/Python/Uni/Thesis/Thesis - code/Data/processed-data/processed_gaze_data.csv'
+TRAIN_FEATURES_PATH = '/Users/sofie/dev/Python/Uni/Thesis/Thesis - code/Data/processed-data/train_windows_features.csv'
 
 # Model parameters
-XGB_FINAL = {}
 XGB_PARAMS = {'eta': [0.01, 0.05, 0.1, 0.3],
               'n_estimators': [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000],
               'max_depth': [3, 5, 7, 9, 12],
@@ -23,15 +24,13 @@ XGB_PARAMS = {'eta': [0.01, 0.05, 0.1, 0.3],
               'objective': 'binary:logistic',
               'tree_method': "hist"}
 
-LR_FINAL = {}
 LR_PARAMS = {'penalty': 'l2',
              'C': 1.0,
              'max_iterations': [100, 200, 500],
              'solver': ['liblinear', 'newton-cg', 'lbfgs', 'saga']}
 
-RF_FINAL = {}
 RF_PARAMS = {}
 
-LSVM_FINAL = {}
 LSVM_PARAMS = {'C': [0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000],
                'max_iter': 1000}
+
