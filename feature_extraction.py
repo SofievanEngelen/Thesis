160bdf20c3334cfdc317dba9b60d51d783522e60
SofievanEngelen <128477751+SofievanEngelen@users.noreply.github.com>
2024-10-22 17:51:30 +0100

    refactor feature_extraction.py and drop windows if fixation lasts longer than given duration

--- feature_extraction.py
+++ feature_extraction.py
@@ -3,19 +3,18 @@ import pandas as pd
 import numpy as np
 from scipy.stats import kurtosis, skew
 from saccades import detect_events
+from constants import AOI_DF_PATH, MIN_FIXATION_DURATION
 
-AOI_df = pd.read_csv('/Users/sofie/dev/Python/Uni/Thesis/Thesis - code/Data/original-data/AOI_gaze_data.csv', delimiter=';', decimal=',')
+dropped_windows = []
 
 
 # Helper function that computes statistics for a measure
 def compute_statistics(series, variable_name):
-    """
-        Computes various statistical measures for a given pandas Series.
+    """Compute statistical measures for a pandas Series."""
+    if series.empty:
+        return {f'{variable_name}_{stat}': np.nan for stat in
+                ['mean', 'median', 'std', 'max', 'min', 'range', 'kurtosis', 'skewness']}
 
-        :param series: A pandas Series containing numerical Data.
-        :param variable_name: The name of the variable to compute statistics for, for dictionary naming purposes.
-        :return: A dictionary with statistical measures.
-    """
     return {
         f'{variable_name}_mean': series.mean(),
         f'{variable_name}_median': series.median(),
@@ -37,55 +36,50 @@ def compute_features(gaze_data: pd.DataFrame) -> pd.DataFrame:
     """
     # Detect events from gaze Data
     fixation_df, saccade_df = detect_events(gaze_data)
-    # print("Blinks: ", blink_df)
 
-    participant = gaze_data['Participant'][0]
-    paragraph = gaze_data['Paragraph'][0]
+    # Drop the window if any fixation lasts more than 10 seconds
+    if not fixation_df.empty and fixation_df['duration'].max() > MIN_FIXATION_DURATION:
+        return pd.DataFrame()
 
-    fixation_df['participant'] = participant
-    # blink_df['participant'] = participant
-    saccade_df['participant'] = participant
+    participant = gaze_data['Participant'].iloc[0]
+    paragraph = gaze_data['Paragraph'].iloc[0]
+    trial = gaze_data['trial'].iloc[0]
 
     fixation_df['paragraph'] = paragraph
-    # blink_df['paragraph'] = paragraph
-    saccade_df['paragraph'] = paragraph
 
-    # print("Fixations: ", fixation_df)
-    # print("Saccades: ", saccade_df)
+    # Extract AOI coordinates for paragraph
+    AOI_df = pd.read_csv(AOI_DF_PATH, delimiter=';', decimal=',')
+    aoi_coords = AOI_df.loc[AOI_df['Paragraph'] == paragraph, ['x1', 'y1', 'x2', 'y2']].iloc[0]
+    aoi_tl_x, aoi_tl_y, aoi_br_x, aoi_br_y = aoi_coords
 
     # Filter fixations that fall within the AOI
-    fixation_AOI_df = fixation_df[fixation_df.apply(lambda row: checkAOI(row['x_CC'], row['y_CC'], row['paragraph']),
-                                                    axis=1)]
+    fixation_AOI_df = fixation_df[
+        (fixation_df['x'].between(aoi_tl_x, aoi_br_x)) &
+        (fixation_df['y'].between(aoi_br_y, aoi_tl_y))
+        ]
 
-    # print("AOI fixations: ", fixation_AOI_df)
-
-    # Define features to avoid repeated lookups
     num_fixations = len(fixation_df)
     num_saccades = len(saccade_df)
-    saccade_angles = saccade_df['angle']
-    # blink_duration = blink_df['duration'] if not blink_df.empty else pd.Series([0])
 
-    # Fixation features
+    # Compute fixation features
     fixation_duration_stats = compute_statistics(fixation_df['duration'], 'fixation_duration')
     fixation_dispersion_stats = compute_statistics(fixation_df['dispersion'], 'fixation_dispersion')
 
-    # Saccade features
+    # Compute saccade features
+    saccade_angles = saccade_df['angle']
     saccade_duration_stats = compute_statistics(saccade_df['duration'], 'saccade_duration')
     saccade_amplitude_stats = compute_statistics(saccade_df['amplitude'], 'saccade_amplitude')
     saccade_angle_stats = compute_statistics(saccade_angles, 'saccade_angle')
 
-    # Blink features
-    # blink_duration_stats = compute_statistics(blink_duration, 'blink_duration')
-
-    # Fixation within AOI features
-    if not fixation_AOI_df.empty:
-        fixation_AOI_duration_stats = compute_statistics(fixation_AOI_df['duration'], 'fixation_AOI_duration_')
-        fixation_AOI_dispersion_stats = compute_statistics(fixation_AOI_df['dispersion'], 'fixation_AOI_dispersion')
-    else:
-        fixation_AOI_duration_stats = compute_statistics(pd.Series(0), 'fixation_AOI_duration_')
-        fixation_AOI_dispersion_stats = compute_statistics(pd.Series(0), 'fixation_AOI_dispersion')
+    # Compute fixation AOI features
+    fixation_AOI_duration_stats = compute_statistics(fixation_AOI_df['duration'], 'fixation_AOI_duration_')
+    fixation_AOI_dispersion_stats = compute_statistics(fixation_AOI_df['dispersion'], 'fixation_AOI_dispersion')
 
     feature_dict = {
+        'participant': participant,
+        'paragraph': paragraph,
+        'trial': trial,
+
         'num_fixations': num_fixations,
         **fixation_duration_stats,
         **fixation_dispersion_stats,
@@ -94,33 +88,12 @@ def compute_features(gaze_data: pd.DataFrame) -> pd.DataFrame:
         **saccade_duration_stats,
         **saccade_amplitude_stats,
         **saccade_angle_stats,
-        'horizontal_saccade_ratio': (saccade_angles.abs() < (np.pi / 4)).sum() / num_saccades if num_saccades else np.nan,
+        'horizontal_saccade_ratio': (saccade_angles.abs() < (np.pi / 4)).mean() if num_saccades else np.nan,
         'fixation_saccade_ratio': num_fixations / num_saccades if num_saccades else np.nan,
 
-        # 'num_blinks': len(blink_df),
-        # **blink_duration_stats,
         'num_fixations_AOI': len(fixation_AOI_df),
         **fixation_AOI_duration_stats,
         **fixation_AOI_dispersion_stats
     }
 
-    return pd.DataFrame.from_dict(feature_dict, orient='index').T
-
-
-def checkAOI(x: float, y: float, paragraph: int) -> bool:
-    """
-    Checks if a given gaze point (x, y) is in the AOI (area of interest) of the given paragraph. Coordinates in
-    Cartesian coordinate system.
-
-    :param x: X coordinate of the gaze point.
-    :param y: Y coordinate of the gaze point.
-    :param paragraph: Paragraph in which the gaze point occurs.
-    :return: Boolean indicating if the gaze point is within the AOI of the given paragraph.
-    """
-    aoi_tl_x = float(AOI_df.loc[AOI_df['Paragraph'] == paragraph, 'x1'].iloc[0])
-    aoi_tl_y = float(AOI_df.loc[AOI_df['Paragraph'] == paragraph, 'y1'].iloc[0])
-    aoi_br_x = float(AOI_df.loc[AOI_df['Paragraph'] == paragraph, 'x2'].iloc[0])
-    aoi_br_y = float(AOI_df.loc[AOI_df['Paragraph'] == paragraph, 'y2'].iloc[0])
-
-    return aoi_tl_x <= x <= aoi_br_x and aoi_br_y <= y <= aoi_tl_y
-
+    return pd.DataFrame([feature_dict])
